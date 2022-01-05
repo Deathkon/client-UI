932023a37831a9b180688abeb51ea25ff87cf7ad
Deathkon <74615179+Deathkon@users.noreply.github.com>
2022-01-05 18:06:08 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Profile from './PortfilioContainer/Home/Profile';
 function App() {
   return (
     <div className="App">
-      
+      <Profile/>
     </div>
   );
 }
--- src/PortfilioContainer/Home/Profile.js
+++ src/PortfilioContainer/Home/Profile.js
@@ -1,10 +1,71 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
+import Typical from 'react-typical'
 
 export default function Profile() {
     return (
         <div className='profile-container'>
             <div className='profile-parent'>
-                <div className='profile-header'>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <a href='#'>
+                            <i className='fa fa-facebook-square'></i>
+                        </a>
+                        <a href='https://www.linkedin.com/in/m-philbert-a13776226/'>
+                            <i className='fa fa-linkedin-square'></i>
+                        </a>
+                        <a href='https://www.instagram.com/govirtual1/'>
+                            <i className='fa fa-instagram-square'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-twitter-square'></i>
+                        </a>
+                        <a href='https://github.com/Deathkon'>
+                            <i className='fa fa-github-square'></i>
+                        </a>
+                    </div>
+                    <div className='profile-details-name'>
+                        <span className='primary-text'>
+                            {" "}
+                            Hello, I'M <span className='highlighted-text'>Philbert</span>
+                        </span>
+                    </div>
+                    <div className='profile-details-role'>
+                        <span className='primary-text'>
+                            {" "}
+                            <h1>
+                                {" "}
+                                <Typical>
+                                    loop={Infinity}
+                                    steps={[
+                                        'Enthusiastic Dev 😎',
+                                        1000,
+                                        'Full Stack Developer 💻',
+                                        1000,
+                                        'Java Developer 📱',
+                                        1000,
+                                        'Cross Platform Dev 🔴',
+                                        1000,
+                                        'React/React Native Dev 🌐',
+                                        1000,
+                                    ]}
+                                </Typical>
+                            </h1>
+                            <span className='profile-role-tagline'>
+                                {" "}
+                                I'm a Full Stack Developer with a passion for building beautiful, interactive, and user-friendly web platform and applications.
+                            </span>
+                        </span>
+                    </div>
+                    <div className='profile-options'>
+                    <button className='btn primary-btn'>
+                        {" "}
+                        Hire Me{" "}
+                    </button>
+                    <a href='resume.pdf' download='Philbert resume.pdf'>
+                        <button className='btn highlighted-btn'>Get Resume</button>
+                    </a>
+                    </div>
                 </div>
             </div>
         </div>
