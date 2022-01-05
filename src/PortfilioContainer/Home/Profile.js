ebdf4424f11eab7ef7444ba48cc333541ae37921
Deathkon <74615179+Deathkon@users.noreply.github.com>
2022-01-05 17:09:53 +0200

    commit

---
+++ src/PortfilioContainer/Home/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-header'>
+                </div>
+            </div>
+        </div>
+    )
+}
