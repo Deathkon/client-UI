bb58c534165a83ac7930fa070db2dc411bda4f27
Deathkon <74615179+Deathkon@users.noreply.github.com>
2022-01-06 13:49:23 +0200

    commit

--- src/PortfilioContainer/Home/Footer/Footer.js
+++ src/PortfilioContainer/Home/Footer/Footer.js
@@ -5,7 +5,9 @@ export default function Footer() {
     return (
         <div className='footer-container'>
             <div className='footer-parent'>
-            <img src={require('../../../assets/Home/shape-bg.png').default} alt='No Internet Connection'/>
+                <img 
+                src={require("../../../assets/Home/shape-bg.png")} 
+                alt='No Internet Connection' />
             </div>
         </div>
     )
