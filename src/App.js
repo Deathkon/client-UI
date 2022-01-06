5c1cec554bcd18cadd6553539b23889468404bfd
Deathkon <74615179+Deathkon@users.noreply.github.com>
2022-01-06 13:28:08 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Profile from './PortfilioContainer/Home/Profile';
+import Footer  from './PortfilioContainer/Home/Footer/Footer';
 function App() {
   return (
     <div className="App">
       <Profile/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/PortfilioContainer/Home/Footer/Footer.js
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import './Footer.css'
+export default function Footer() {
+    return (
+        <div className='footer-container'>
+            <div className='footer-parent'>
+            <img src={require('../../../assets/Home/shape-bg.png').default} alt='No Internet Connection'/>
+            </div>
+        </div>
+    )
+}
